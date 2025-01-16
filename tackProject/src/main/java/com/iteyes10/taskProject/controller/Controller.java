0a88c818db2d6ea55b515456ff5d93f0605fdfff
minuk27 <kad3500@naver.com>
2025-01-16 17:02:55 +0900

    김민욱
    프로젝트 이름 수정

---
+++ tackProject/src/main/java/com/iteyes10/taskProject/controller/Controller.java
@@ -0,0 +1,10 @@
+package com.iteyes10.taskProject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Controller
+{
+}
