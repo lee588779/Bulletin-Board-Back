d6c5347f95a9c2f26b34e9940f1894ee7a736a5c
didwogus59 <didwogus59@naver.com>
2025-01-17 09:30:50 +0900

    entity table matching

--- taskProject/src/main/java/com/iteyes10/taskProject/domain/Contents.java
+++ taskProject/src/main/java/com/iteyes10/taskProject/domain/Contents.java
@@ -1,15 +1,38 @@
 package com.iteyes10.taskProject.domain;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Contents {
     @Id
+    private int cid;
+
+    @Column
     private String uid;
+
     @Column
     private String title;
+
     @Column
     private String text;
+    
+    @Column
+    private LocalDate regdt;
+
+    @Column
+    private String status;
+
+    @Column 
+    private String type;
+    
 }
--- taskProject/src/main/java/com/iteyes10/taskProject/domain/User.java
+++ taskProject/src/main/java/com/iteyes10/taskProject/domain/User.java
@@ -1,17 +1,27 @@
 package com.iteyes10.taskProject.domain;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     private String uid;
     @Column
     private String pwd;
     @Column
-    private Long level;
+    private String level;
     @Column
     private String name;
+    @Column
+    private LocalDate regdt;
 }
